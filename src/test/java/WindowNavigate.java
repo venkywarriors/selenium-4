d90d083e08edef28278fddb438cfde54ebb372d8
Venkateshwara Rao Doijode <33220328+venkywarriors@users.noreply.github.com>
2021-01-16 20:08:32 +0530

    Update and rename NavigateTest.java to WindowNavigate.java

--- src/test/java/WindowNavigate.java
+++ src/test/java/WindowNavigate.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NavigateTest {
+public class WindowNavigate {
 
   @Test
   public void openNewTab(){
