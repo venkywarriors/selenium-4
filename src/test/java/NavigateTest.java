f85ea064fbbf2b957d587d99c5a0e776e3d50865
Venkateshwara Rao Doijode <33220328+venkywarriors@users.noreply.github.com>
2021-01-16 18:23:33 +0530

    Update NavigateTest.java

--- src/test/java/NavigateTest.java
+++ src/test/java/NavigateTest.java
@@ -17,7 +17,6 @@ public class NavigateTest {
     driver.navigate().to("https://www.swtestacademy.com");
   }
 
-
   @Test
   public void openNewWindow(){
 
@@ -27,5 +26,38 @@ public class NavigateTest {
     driver.navigate().to("https://www.google.com");
     driver.switchTo().newWindow(WindowType.WINDOW);
     driver.navigate().to("https://www.swtestacademy.com");
+  }
+   @Test
+  public void switchOptions(){
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.google.com/");
+
+            // open a new window
+            driver.switchTo().newWindow(WindowType.WINDOW);
+            //navigate to a URL on newly opened window
+            driver.navigate().to("https://www.youtube.com/");
+
+            // get the window ids to switch between them
+            Set<String> windows = driver.getWindowHandles();
+            Iterator<String> it = windows.iterator();
+            String googleWindow = it.next();
+            String youtubeWindow = it.next();
+            System.out.println(driver.getTitle());
+
+            // switch the control to the google window
+            driver.switchTo().window(googleWindow);
+            System.out.println(driver.getTitle());
+    
+       // open a new tabbed window
+            driver.switchTo().newWindow(WindowType.TAB);
+            //navigate to a URL on newly opened tabbed window
+            driver.navigate().to("https://www.gmail.com/");
+            System.out.println(driver.getTitle());
+
+            driver.switchTo().window(youtubeWindow);
+            System.out.println(driver.getTitle());
+
+            driver.quit();
   }
 }
